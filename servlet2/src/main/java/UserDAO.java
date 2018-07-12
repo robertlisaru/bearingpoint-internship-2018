bda0ebe2ff620bfdbb0e019e59ff3251044b5767
Laurentiu Simion <laurentiu394@gmail.com>
2018-07-12 14:41:53 +0300

    Servlet

---
+++ servlet2/src/main/java/UserDAO.java
@@ -0,0 +1,75 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserDAO {
+
+	
+	private Connection con = null;
+	private Statement stm = null;
+	private PreparedStatement pstm = null;
+	private ResultSet rs = null;
+	static final String DATABASE_URL = "jdbc:mysql://localhost:3306/useraccess";
+
+	
+	public void  insert(User newUser)
+	{
+		try {
+
+			connect();
+			
+			String sql = "INSERT INTO users (username,pass,email,gender) VALUES(?,?,?,?)";
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, newUser.getUsername());
+			pstm.setString(2, newUser.getPassword());
+			pstm.setString(3, newUser.getEmail());
+			pstm.setString(4, newUser.getGender());
+		
+			pstm.executeUpdate();
+			pstm.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+		close();
+		}
+		
+	}
+	
+	
+
+		
+		private void connect(){
+
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection(DATABASE_URL,"root","");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	private void close(){
+		
+			try {
+				if(rs!=null)
+					rs.close();
+				if(stm!=null)
+					stm.close();
+				if(pstm!=null)
+					pstm.close();
+				if(con!=null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+}
---
+++ servlet2/src/main/java/register.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginCheck
+ */
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String pass = request.getParameter("pass");
+		String email = request.getParameter("email");
+		String gender = request.getParameter("gender");
+		User user = new User(username, pass, email, gender);
+		new UserDAO().insert(user);
+
+		response.sendRedirect("index.jsp");
+
+		
+		
+	}
+
+}
